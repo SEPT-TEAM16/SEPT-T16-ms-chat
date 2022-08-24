84884479bec273ba796d025d8364f063af2370f9
Daniel <s3786266@student.rmit.edu.au>
2022-08-24 19:25:19 +1000

    Created Gradle Project structure

---
+++ src/main/java/com/rmit/sept/mschat/MsChatApplication.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.mschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rmit/sept/mschat/MsChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.mschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
