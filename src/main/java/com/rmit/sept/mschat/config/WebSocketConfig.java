67b4730f96f5de92afadac212df34db7705b15f4
Daniel <s3786266@student.rmit.edu.au>
2022-10-08 13:40:04 +1100

    created custom websocket config

---
+++ src/main/java/com/rmit/sept/mschat/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.rmit.sept.mschat.config;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/chatroom", "/user");
+        registry.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+
+        // declaring the endpoint and its allowed origin
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+}
